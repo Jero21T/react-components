0e4dfce6199e71a7df36aa53a16000479e87d729
Jeroen <jeroent94@gmail.com>
2017-12-13 14:31:39 -0800

    halfway

--- app.js
+++ app.js
@@ -1,1 +1,54 @@
 // TODO
+var GroceryList = () => (
+  <GroceryListItem items = {['A','B','C']}/>
+);
+
+var GroceryListItem = (props) =>(
+ <ul>
+    <li>{props.items[0]}</li>
+    <li>{props.items[1]}</li>
+    <li>{props.items[2]}</li>
+  </ul>
+)
+
+ReactDOM.render(<GroceryList />, document.getElementById("app"));
+
+
+// A class component can be defined as an ES6 class
+// that extends the base Component class included in the React library
+class GroceryListItem extends React.Component {
+
+  // A `constructor` method is expected on all ES6 classes
+  // When React instantiates the component,
+  // it will pass `props` to the constructor
+  constructor(props) {
+    // Equivalent to ES5's React.Component.call(this, props)
+    super(props);
+  }
+
+  // Every class component must have a `render` method
+  // Stateless functional components are pretty much just this method
+  render() {
+
+    // `props` is no longer passed as an argument,
+    // but instead accessed with `this.props`
+    return {(
+      <li>{this.props.item}</li>
+    );
+}
+  }
+
+}
+
+
+var GroceryList = (props) => (
+  <ul>
+    {props.items.map(item =>
+      <GroceryListItem item={item} />
+    )}
+  </ul>
+);
+
+ReactDOM.render(<GroceryList />, document.getElementById("app"));
+
+
